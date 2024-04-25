15b100202d3f1e83ec377cad2c40c893435fdc73
Torben Virtmann <Torben.Virtmann@sva.de>
2024-04-25 16:55:10 +0200

    changes

--- software/b8.asm
+++ software/b8.asm
@@ -7,16 +7,26 @@
 	#bits 8
 }
 
+
 #subruledef flag
 {
-    0 => 0
-    1 => 1
-    z => 2
-    nz => 3
-    eq => 4
-    neq => 5
-    s => 6
-    ns => 7
+	0 => 0
+	1 => 1
+	z => 2
+	nz => 3
+	eq => 4
+	neq => 5
+	s => 6
+	ns => 7
+
+	d0 => 8
+	nd0 => 9
+	d1 => 10
+	nd1 => 11
+	d2 => 12
+	nd2 => 13
+	d3 => 14
+	nd3 => 15
 }
 
 #ruledef
@@ -42,34 +52,39 @@
 	; Address Nand
 	nand [{address: u12}] => 0xB @ address
 	
+	; cmp without value
+	cmp {f: flag} => {
+		assert(f == 0)
+		0x4 @ 0x0 @ 0x00
+	}
+	cmp {f: flag} => {
+		assert(f == 1)
+		0x4 @ 0x1 @ 0x00
+	}
+	
+	; Compare Is Condition
+	cmp {f: flag},{value: u8} => {
+		assert(f & 1 == 0)
+		0x4 @ f`4 @ value
+	}
+	; Compare Is Not Condition
+	cmp {f: flag},{value: u8} => {
+		assert(f & 1 == 1)
+		0xC @ f`4 @ value
+	}
+	
+	; Address Store Program Counter
+	stp [{address: u12}] => 0xD @ address
+	
 	; Immediate Jump
 	jp {address: u12} => 0x4 @ address
 	
 	; Address Jump
 	jp [{address: u12}] => 0xC @ address
 	
-	; Address Store Program Counter
-	stp [{address: u12}] => 0xD @ address
-	
 	; Immediate Conditional Jump
 	jpc {address: u12} => 0x6 @ address
 	
 	; Address Conditional Jump
 	jpc [{address: u12}] => 0xE @ address
-	
-	; Compare Is Condition
-	cmp {f: flag}, {value: u8} =>
-	{
-        assert(f <= 7)
-		flagCut = (0`4 | (f & 0x7))
-		flagCut`4 @ 0x7`4 @ value`8 
-	}
-	
-	; Compare Is Not Condition
-	cmp {f: flag}, {value: u8} =>
-	{
-        assert(f <= 7)
-		flagCut = (0x8`4 | (f & 0x7))
-		flagCut`4 @ 0xF`4 @ value`8 
-	}
 }
