cc4b72bb8031bc9d746f63021223eecdf2e4f1c5
Pixel Brush <letsplaytvirtmann@gmail.com>
2024-05-01 12:26:23 +0200

    microcode change
    this more closely resembles what ben eater does in his sap-1 computer build, and just makes more sense than the system i was trying to hack together, especially since I'll most likely be using an EEPROM or ROM of some kind for my microcode.

--- software/micro/micro.asm
+++ software/micro/micro.asm
@@ -1,50 +1,34 @@
-#once
-#bankdef b4
-{
-    #addr 0x0
-    #addr_end 0xf
-    #outp 4 * 0x0
-	#bits 4
-}
-
+; lower three bits for register
+; if last bit is set, then special function 
 # ruledef register
 {
-    pb => 0x0
-    sb => 0x1
-    a => 0x2
-    cond => 0x3
-    pc => 0x4
-    pcs => 0x5
-    aluAdd => 0x6
-    aluNand => 0x7
+	none => 0x0
+	m => 0x1 ; memory
+    p => 0x2 ; primary buffer
+    s => 0x3 ; secondary buffer
+    a => 0x4 ; accumulator
+    alu => 0x5 ; alu output
+    pc => 0x6 ; program counter
+    pcs => 0x7 ; program counter storage
+	ps => 0x8 ; combined pb and sb
+	
+	; increment pc
 }
 
 #ruledef
 {
-    ; 0 load
-    ld {r: register} => 0x0`1 @ r`3
-    ; 1 execute
-    ex {r: register} => 0x1`1 @ r`3
+    ; 0 output ro, input ri
+    o{ro: register} i{ri:register}=> 0x0`1 @ ro`3 @ 0x0`1 @ ri`3
 }
-
-; Load Immediate
-#align 4
-ldImm:
-ld pb
-ex pc
-ld sb
-ex pc
-ld a
 
 ; Load from Address
 #align 4
 ldAddr:
-ld pb
-ex pc
-ld sb
-ex pc
-; same everywhere
-ld pcs ; load pc into pcs
-ld pc ; load sb + pb into pc
-
-ld a
+om ip
+;inc pc
+om is
+;inc pc
+opc ipcs
+ops ipc
+om ia
+opcs ipc
