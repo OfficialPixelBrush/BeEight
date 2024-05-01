9a6e0dc8e265a3f286fb63a2adc18ec819ca4458
Pixel Brush <letsplaytvirtmann@gmail.com>
2024-05-01 12:38:25 +0200

    aqwrdawsfrd

--- software/micro/micro.asm
+++ software/micro/micro.asm
@@ -2,33 +2,43 @@
 ; if last bit is set, then special function 
 # ruledef register
 {
-	none => 0x0
+	;registers
+	; none => 0x0
 	m => 0x1 ; memory
     p => 0x2 ; primary buffer
     s => 0x3 ; secondary buffer
     a => 0x4 ; accumulator
-    alu => 0x5 ; alu output
+    alu => 0x5 ; alu (output determined by instruction)
     pc => 0x6 ; program counter
     pcs => 0x7 ; program counter storage
-	ps => 0x8 ; combined pb and sb
-	
-	; increment pc
+	; 0x8 reset instruction counter
+	; 0x9
+	; 0xA
+	; 0xB
+	; 0xC
+	; 0xD increment pc
+	; 0xF
 }
 
 #ruledef
 {
     ; 0 output ro, input ri
-    o{ro: register} i{ri:register}=> 0x0`1 @ ro`3 @ 0x0`1 @ ri`3
+    o{ro: register} i{ri:register}=> ro`4 @ ri`4
+	; reset instruction counter
+	nxt => 0x8`4 @ 0x0`4
+	; increment pc
+	inc => 0xD`4 @ 0x0`4
 }
 
 ; Load from Address
 #align 4
 ldAddr:
 om ip
-;inc pc
+inc
 om is
-;inc pc
+inc
 opc ipcs
-ops ipc
+opcs ipc
 om ia
 opcs ipc
+nxt ; <- Interrupt opportunity?
